0a0285e25e17c883c98a1d95789d86c05e60493d
Julien Aubert <j.aubert@actongroup.com>
2022-04-27 23:59:10 +0200

    add queue

--- src/main/java/fr/gouv/culture/francetransfert/core/enums/RedisQueueEnum.java
+++ src/main/java/fr/gouv/culture/francetransfert/core/enums/RedisQueueEnum.java
@@ -1,11 +1,13 @@
 package fr.gouv.culture.francetransfert.core.enums;
 
 public enum RedisQueueEnum {
-	MAIL_QUEUE("email-notification-queue"),MAIL_NEW_RECIPIENT_QUEUE("email-new-recipient-notification-queue"), ZIP_QUEUE("zip-worker-queue"),
-	DOWNLOAD_QUEUE("download-notification-queue"), TEMP_DATA_CLEANUP_QUEUE("redis-temp-data-cleanup-queue"),
+	MAIL_QUEUE("email-notification-queue"), MAIL_NEW_RECIPIENT_QUEUE("email-new-recipient-notification-queue"),
+	ZIP_QUEUE("zip-worker-queue"), DOWNLOAD_QUEUE("download-notification-queue"),
+	TEMP_DATA_CLEANUP_QUEUE("redis-temp-data-cleanup-queue"),
 	CONFIRMATION_CODE_MAIL_QUEUE("confirmation-code-mail-queue"), TTL_CODE_CONFIRMATION("ttl-code-confirmaton"),
 	SATISFACTION_QUEUE("satisfaction-queue"), STAT_QUEUE("stat-queue"), SEQUESTRE_QUEUE("sequestre-queue"),
-	FORMULE_CONTACT_QUEUE("formule-contact-queue"), DELETE_RECIPIENT("delete-recipient");
+	FORMULE_CONTACT_QUEUE("formule-contact-queue"), DELETE_RECIPIENT("delete-recipient"),
+	DELETE_ENCLOSURE_QUEUE("delete-enclosure-queue");
 
 	private String value;
 
