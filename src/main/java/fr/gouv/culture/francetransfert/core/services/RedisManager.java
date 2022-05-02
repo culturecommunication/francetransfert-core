bbcb3962364fd066a7f98212b01804e53df055c4
Julien Aubert <j.aubert@actongroup.com>
2022-05-02 13:34:57 +0200

    add lrem command

--- src/main/java/fr/gouv/culture/francetransfert/core/services/RedisManager.java
+++ src/main/java/fr/gouv/culture/francetransfert/core/services/RedisManager.java
@@ -1369,6 +1369,26 @@ public class RedisManager {
 		return ret;
 	}
 
+	public long lrem(String key, long count, String value) {
+		Jedis jedis = null;
+		boolean success = true;
+		long ret = 0L;
+		try {
+			jedis = pool.getResource();
+			if (jedis == null) {
+				success = false;
+				throw new JedisException(JEDIS_NULL);
+			}
+			ret = jedis.lrem(key, count, value);
+		} catch (Exception e) {
+			success = false;
+			returnBrokenResource(jedis, "lrem" + key, e);
+		} finally {
+			releaseRedisSource(success, jedis);
+		}
+		return ret;
+	}
+
 	protected String getHost() {
 		return host;
 	}
