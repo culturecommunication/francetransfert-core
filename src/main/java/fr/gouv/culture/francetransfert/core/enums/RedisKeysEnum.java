0bc3eea39b8e7cb1dc60c527798ab9073be28e71
kfifat abir <a.kfifat@actongroup.com>
2022-05-20 11:21:37 +0200

    Download date (admin)

--- src/main/java/fr/gouv/culture/francetransfert/core/enums/RedisKeysEnum.java
+++ src/main/java/fr/gouv/culture/francetransfert/core/enums/RedisKeysEnum.java
@@ -13,7 +13,8 @@ public enum RedisKeysEnum {
 	FT_CODE_SENDER("sender:", ":confirmation-email-code"), FT_CODE_TRY("sender:", ":code-sender-try"),
 	FT_TOKEN_SENDER("sender:", ":token"), FT_SENDER_PLIS("sender:", ":plis"),
 	FT_DOMAINS_MAILS_MAILS("enclosure-mails:mails", ""), FT_DOMAINS_MAILS_TMP("enclosure-mails:tmp", ""),
-	CHECK_MAIL("check-mail", ""), FT_SEND("send:", ""), FT_RECEIVE("receive:", "");
+	CHECK_MAIL("check-mail", ""), FT_SEND("send:", ""), FT_RECEIVE("receive:", ""),
+	FT_Download_Date("recipient:", ":dates");
 
 	private String firstKeyPart;
 	private String lastKeyPart;
