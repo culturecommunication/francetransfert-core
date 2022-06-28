b261047f9ae33d0c04f31a46b475d9374b9b4cf4
kfifat abir <a.kfifat@actongroup.com>
2022-06-28 10:02:57 +0200

    historique des plis - ajout d'une nouvelle langue

--- src/main/java/fr/gouv/culture/francetransfert/core/enums/EnclosureKeysEnum.java
+++ src/main/java/fr/gouv/culture/francetransfert/core/enums/EnclosureKeysEnum.java
@@ -9,7 +9,7 @@ public enum EnclosureKeysEnum {
 	PASSWORD_GENERATED("password-generated"), MESSAGE("message"), SUBJECT("subject"),
 	UPLOAD_NB_FILES_DONE("upload-nb-files-done"), PUBLIC_LINK("public-link"),
 	PUBLIC_DOWNLOAD_COUNT("public-download-count"), TOKEN("token"), HASH_FILE("hash-file"), LANGUAGE("language"),
-	PASSWORD_ZIP("zip-password"), DELETED("deleted");
+	PASSWORD_ZIP("zip-password"), DELETED("deleted"), EXPIRED_TIMESTAMP_ARCHIVE("expired-timestamp-archive");
 
 	private String key;
 
