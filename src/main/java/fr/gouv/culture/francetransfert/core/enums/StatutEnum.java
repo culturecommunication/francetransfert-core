83513233dbc2f1a1be11d2de2af74dbba1f602e3
kfifat abir <a.kfifat@actongroup.com>
2022-07-12 10:39:20 +0200

    core modification

---
+++ src/main/java/fr/gouv/culture/francetransfert/core/enums/StatutEnum.java
@@ -0,0 +1,45 @@
+/* 
+ * Copyright (c) Ministère de la Culture (2022) 
+ * 
+ * SPDX-License-Identifier: Apache-2.0 
+ * License-Filename: LICENSE.txt 
+ */ 
+ 
+/*
+  * Copyright (c) Ministère de la Culture (2022) 
+  * 
+  * SPDX-License-Identifier: Apache-2.0 
+  * License-Filename: LICENSE.txt 
+  */
+
+package fr.gouv.culture.francetransfert.core.enums;
+
+public enum StatutEnum {
+
+	EN_COURS("code","libelle") ;
+
+
+	StatutEnum(String key, String value) {
+		this.setKey(key);
+		this.setValue(value);
+	}
+
+	public String getKey() {
+		return key;
+	}
+	public String getValue() {
+		return value;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	private String key;
+	private String value;
+	
+}
