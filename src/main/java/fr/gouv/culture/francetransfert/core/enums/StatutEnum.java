eca595a5b039dc914f04dc67fdd6dda23f2bd016
kfifat abir <a.kfifat@actongroup.com>
2022-08-17 12:30:38 +0200

    api changes

--- src/main/java/fr/gouv/culture/francetransfert/core/enums/StatutEnum.java
+++ src/main/java/fr/gouv/culture/francetransfert/core/enums/StatutEnum.java
@@ -10,11 +10,20 @@ package fr.gouv.culture.francetransfert.core.enums;
 //---
 public enum StatutEnum {
 
-	ECH("011-ECH", "Erreur lors du chargement du pli"), CHT("012-CHT", "Chargement terminé"),
-	ECC("010-ECC", "En cours de chargement"), INI("000-INI", "Initialisé"), EDC("040-EDC", "Envoi des courriels"),
-	PAT("042-PAT", "Prêt au téléchargement"), EEC("041-EEC", "Erreur lors de l’envoi des courriels"),
-	AAV("030-AAV", "Analyse antivirale"), EAV("031-EAV", "Erreur détectée lors de l’analyse antivirale"),
-	APT("032-APT", "Analyse antivirale du pli terminée");
+	
+	INI("000-INI", "Initialisé"), 
+	ECC("010-ECC", "En cours de chargement"),
+	ECH("011-ECH", "Erreur lors du chargement du pli"), 
+	CHT("012-CHT", "Chargement terminé"),
+	ANA("020-ANA", "Analyse du pli"),
+	ETF("021-ETF", "Erreur détectée lors de l’analyse des types de fichier"),
+	EAV("022-EAV", "Erreur détectée lors de l’analyse antivirale"),
+	APT("023-APT", "Analyse du pli terminée"),
+	EDC("030-EDC", "Envoi des courriels"),
+	EEC("031-EEC", "Erreur lors de l’envoi des courriels"),	
+	PAT("032-PAT", "Prêt au téléchargement");	
+	
+	
 
 	StatutEnum(String code, String word) {
 		this.setCode(code);
